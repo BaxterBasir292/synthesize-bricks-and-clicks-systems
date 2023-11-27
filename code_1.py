6989b9d576d4736ad50c6c5dd4f5c9b6c3b56fec
Basir <140265607+BaxterBasir292@users.noreply.github.com>
2023-11-27 20:31:58 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,25 @@
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-        for (int item : data) {
 
+        print(f"Random Number: {item}")
+def main():
+    return data
+import random
+if __name__ == "__main__":
+def generate_random_data():
+    main()
+    for item in data:
+
+    data = generate_random_data()
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
         int[] data = new int[10];
-        }
-
-            System.out.println("Random Number: " + item);
-}
+        for (int item : data) {
     public static void main(String[] args) {
-public class RandomDataGenerator {
-        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
     }
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+}
+        }
+        for (int i = 0; i < 10; i++) {
+        }
